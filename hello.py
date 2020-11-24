e0e38f03330b92853ee9fcc6ccbce9e09346d40a
Matthias Vandermaesen <matthias.vandermaesen@ugent.be>
2020-11-24 13:28:29 +0100

    Added: foo bar

--- hello.py
+++ hello.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 def hello_world():
     name = "Simon"
     return render_template('hello.html', name=name)
+
+@app.route('/foo/bar')
+def foo_bar():
+    return render_template("foobar.html")
